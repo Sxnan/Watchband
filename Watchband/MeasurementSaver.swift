64d7e0a1ce809df24f03fda1a232edfa7a02a036
Sxnan <suxuannan95@gmail.com>
2018-04-04 18:38:50 -0400

    add measurementsaver

---
+++ Watchband/MeasurementSaver.swift
@@ -0,0 +1,18 @@
+//
+//  MeasurementSaver.swift
+//  Watchband
+//
+//  Created by Xuannan Su on 3/29/18.
+//  Copyright © 2018 Xuannan Su. All rights reserved.
+//
+
+import UIKit
+
+struct Measurement {
+    var time: CVTimeStamp
+    var value: Int32
+}
+
+class MeasurementSaver: NSObject {
+    
+}
