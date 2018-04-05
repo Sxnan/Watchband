89160160f4c3b89f45f5a0fe0ecd1eb5bfe01f96
Sxnan <suxuannan95@gmail.com>
2018-04-05 16:40:10 -0400

    update

--- Watchband/MeasurementSaver.swift
+++ Watchband/MeasurementSaver.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-struct Measurement {
-    var time: CVTimeStamp
+struct Measurement: CustomStringConvertible {
+    var time: String
     var value: Int32
+    
+    var description: String {
+        return "\(time), \(value)"
+    }
 }
 
 class MeasurementSaver: NSObject {
+    var dir: URL?
+    var fileUrl: URL?
     
+    override init() {
+        dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+    }
+    
+    func openFile(_ fileName: String) {
+        if let _dir = dir {
+            fileUrl = _dir.appendingPathComponent(fileName)
+        } else {
+            print("File open failed\n")
+        }
+    }
+    
+    func saveMeasurement(_ measurement: Measurement) -> Bool {
+        do {
+            if let _fileUrl = fileUrl {
+                let text = "\(measurement)"
+                try text.write(to: _fileUrl, atomically: false, encoding: .utf8)
+            }
+        } catch {
+            print("Measurement save failed\n")
+            return false
+        }
+        return true
+    }
 }
